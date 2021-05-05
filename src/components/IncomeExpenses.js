9892ef05248cbdbce5b4b7ddce7ae8b33a5c6041
Marco Barrera <marcobarrera749@gmail.com>
2021-05-04 22:23:22 -0700

    Changed class to classname

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const IncomeExpenses = () => {
 	return (
-		<div class="inc-exp-container">
+		<div className="inc-exp-container">
 			<div>
 				<h4>Income</h4>
 				<p className="money plus">
